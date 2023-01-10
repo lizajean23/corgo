a26d84b7a66793559c1315d4afdd16f9ec9fb937
nkvin22 <nino.kvinikadze.2@btu.edu.ge>
2023-01-11 00:47:16 +0400

    completed auth?!!

--- app/src/main/java/com/example/corgo1/registrationFragments/SignUpFragment.kt
+++ app/src/main/java/com/example/corgo1/registrationFragments/SignUpFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.corgo1.HomeActivity
 import com.example.corgo1.R
 import com.example.corgo1.databinding.FragmentSignupBinding
@@ -30,9 +31,10 @@ class SignUpFragment:Fragment(R.layout.fragment_signup) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val controller = Navigation.findNavController(view)
+
         binding.registration.setOnClickListener{
-
-
 
             val email = binding.registrationEmail.text.toString()
             val password = binding.passwordSignUp.text.toString()
@@ -61,6 +63,13 @@ class SignUpFragment:Fragment(R.layout.fragment_signup) {
             }
 
         }
+
+        binding.backSignIn.setOnClickListener {
+            val action = SignUpFragmentDirections.actionSignUpFragmentToLogInFragment()
+            controller.navigate(action)
+        }
+
+
     }
 
 
