dca05191a11ef1f686da925b260f99266b65177e
nkvin22 <nino.kvinikadze.2@btu.edu.ge>
2023-01-19 20:28:10 +0400

    bolo detalebi :p

--- app/src/main/java/com/example/corgo1/registrationFragments/SignUpFragment.kt
+++ app/src/main/java/com/example/corgo1/registrationFragments/SignUpFragment.kt
@@ -43,35 +43,39 @@ class SignUpFragment:Fragment(R.layout.fragment_signup) {
             val username = binding.usernameSignup.text.toString().trim()
 
 
+            if(email.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty() && username.isNotEmpty()){
+                if(password == repeatPassword && Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length >= 6){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            data.child(firebaseAuth.currentUser?.uid!!).child("username").setValue(username)
+                            Toast.makeText(requireContext(), "Registered Successfully!", Toast.LENGTH_SHORT).show()
+                            FirebaseAuth.getInstance().signOut()
+                        }else{
+                            Toast.makeText(requireContext(), it.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    binding.emailLayout.helperText = "*Enter a valid address"
+                }else if(password.length<6){
+                    binding.passLayout.helperText = "*Minimum 6 characters"
+                }else if(password != repeatPassword){
+                    binding.repeatPassLayout.helperText = "*Passwords do not match"
+                }
+            }else{
+                Toast.makeText(requireContext(), "Please fill in all fields", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        binding.rules.setOnClickListener {
+
             val builder = AlertDialog.Builder(requireContext())
             builder.setTitle("OUR RULES")
                 .setMessage("Corgo is created for sharing friendly animal related content, therefore we ask you to upload pictures and posts accordingly! Please follow the rules, spread positivity and refrain from posting negative content, otherwise  your account will be deleted!")
-                .setPositiveButton("GOT IT") { dialogInterface, it ->
-                    if(email.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty() && username.isNotEmpty()){
-                        if(password == repeatPassword && Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length >= 6){
-                            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                                if(it.isSuccessful){
-                                    data.child(firebaseAuth.currentUser?.uid!!).child("username").setValue(username)
-                                    Toast.makeText(requireContext(), "Registered Successfully!", Toast.LENGTH_SHORT).show()
-                                    FirebaseAuth.getInstance().signOut()
-                                }else{
-                                    Toast.makeText(requireContext(), it.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                            binding.emailLayout.helperText = "*Enter a valid address"
-                        }else if(password.length<6){
-                            binding.passLayout.helperText = "*Minimum 6 characters"
-                        }else if(password != repeatPassword){
-                            binding.repeatPassLayout.helperText = "*Passwords do not match"
-                        }
-                    }else{
-                        Toast.makeText(requireContext(), "Please fill in all fields", Toast.LENGTH_LONG).show()
-                    }
-                   dialogInterface.dismiss()
-                }.show()
-
-
+                .setPositiveButton("GOT IT"){ dialogInterface, it ->
+                  dialogInterface.dismiss()
+                }
+                .show()
         }
 
         binding.backSignIn.setOnClickListener {
